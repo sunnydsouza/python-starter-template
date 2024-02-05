3ee98292010192389ed2fef30b69b36b09242a8a
sunnydsouza <dsouzasunny_1436@yahoo.co.in>
2024-02-05 11:59:18 +0530

    fix: updated commitline.config.js

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     extends: ['@commitlint/config-conventional'],
     rules: {
-      'type-enum': [2, 'always', ['feat', 'fix', 'docs', 'chore', 'style', 'refactor', 'test', 'perf']],
+      'type-enum': [2, 'always', ['feat', 'fix', 'docs', 'chore', 'ci', 'refactor', 'test', 'perf']],
       'scope-enum': [2, 'always', ['ui', 'backend', 'api', 'deployment']],
       'header-max-length': [2, 'always', 100]
     }
